eb5914d5aef80b9090cd089c19d78faae99925d7
risou <risou.f@gmail.com>
2026-05-10 14:18:54 +0900

    feat: wire cli

--- cmd/gofixreport/main.go
+++ cmd/gofixreport/main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"os"
+
+	"github.com/risou/go-fix-report/internal/app"
+	"github.com/risou/go-fix-report/internal/output"
 )
 
 func main() {
-	fmt.Fprintln(os.Stderr, "usage: gofixreport [--json] [path]")
+	jsonOutput := flag.Bool("json", false, "emit machine-readable JSON output")
+	flag.Parse()
+
+	path := "."
+	if flag.NArg() > 1 {
+		fmt.Fprintln(os.Stderr, "usage: gofixreport [--json] [path]")
+		os.Exit(2)
+	}
+	if flag.NArg() == 1 {
+		path = flag.Arg(0)
+	}
+
+	result := app.Run(context.Background(), app.Options{Path: path}, app.Dependencies{})
+
+	var err error
+	if *jsonOutput {
+		err = output.WriteJSON(os.Stdout, result.Report)
+	} else {
+		err = output.WriteTable(os.Stdout, result.Report)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "writing output: %v\n", err)
+		os.Exit(1)
+	}
+	if result.HadErrors {
+		os.Exit(1)
+	}
 }
